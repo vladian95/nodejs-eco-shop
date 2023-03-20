161f0c8b65fbf31b703b2b57a01656d3e1aac37c
vladian95 <kvankvan1995@gmail.com>
2023-03-20 13:11:51 +0300

    ignore

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('hello');
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+mongoose
+  .connect(
+    'mongodb+srv://vlad95:roller_95@cluster0.bexl1u6.mongodb.net/shop?retryWrites=true&w=majority'
+  )
+  .then(() => console.log('DB Connection successfull'))
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(5000, () => {
+  console.log('backend server is running');
+});
